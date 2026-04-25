44e8f3859cddad0beb8b992949bb83644357c662
Rishav <rishavkrmishra004@gmail,com>
2026-04-26 00:25:00 +0530

    fix api url for production

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -5,7 +5,7 @@ function Products() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/products")
+    axios.get(`${process.env.REACT_APP_API_URL}/api/products`)
       .then(res => setProducts(res.data))
       .catch(err => console.log(err));
   }, []);
